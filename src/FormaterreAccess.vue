6d3ab10140c706a7001f3bd21ce4576f930cd2f0
epointal <pointal@ipgp.fr>
2026-09-08 11:50:52 +0200

    habillage

--- src/FormaterreAccess.vue
+++ src/FormaterreAccess.vue
@@ -1,32 +1,45 @@
 <template>
-    <div class="access" v-if="user">
+    <div v-if="asking" class="spinner">
+        <div><font-awesome-icon icon="fa-solid fa-refresh" class="fa-spin" /></div>
+    </div>
+    <div class="message success" v-if="success" @click="success=null" v-html="success"></div>
+    <div class="message error" v-if="error" @click="error=null" v-html="error"></div>
+    <div class="access" v-if="user" @click="resetMessage()">
         <h1>Access request</h1>
         <div>Hello <b>{{ user.email }}</b>!</div>
         <div style="margin-top:10px;display:block;">
+            <template v-if="withInstituion">
             Complete the information about your organization if necessary, then select the access rights you require.
+            </template>
+            <template v-else>
+                Select the access rights you require.
+            </template>
         </div>
-        <h2>Your organisation</h2>
-        <div><label>Name</label> 
-        <input type="hidden" v-model="organisation.id" />
-        <input v-model="organisation.name" type="text" list="organisations" @input="organisationUpdated($event)" required> *
-            <datalist id="organisations">
-                <option v-for="org in organisations" :data-value="org.o_uid" >{{org.o_name}}<span v-if="org.o_short"> ({{org.o_short}})</span></option>
-            </datalist>
-       
-        </div>
-        <div><label>Type</label>
+        <span v-if="withInstitution">
+            <h2>Your organisation</h2>
         
-            <select v-model="organisation.type">
-                <option value="">---</option>
-                <option v-for="tp in types" :value="tp.t_id">{{ tp.t_name }}</option>
-            </select>
-        </div>
+            <div><label>Name</label> 
+            <input type="hidden" v-model="organisation.id" />
+            <input v-model="organisation.name" type="text" list="organisations" @input="organisationUpdated($event)" required> *
+                <datalist id="organisations">
+                    <option v-for="org in organisations" :data-value="org.o_uid" >{{org.o_name}}<span v-if="org.o_short"> ({{org.o_short}})</span></option>
+                </datalist>
+        
+            </div>
+            <div><label>Type</label>
+            
+                <select v-model="organisation.type" :disabled="organisation.id">
+                    <option value="">---</option>
+                    <option v-for="tp in types" :value="tp.t_id">{{ tp.t_name }}</option>
+                </select>
+            </div>
+        </span>
         <h2>Your access right</h2>
-         <template v-for="client in clients">
-            <div><h4>{{ client.name}}</h4>
-                <div class="input-role" v-for="role in roles[client.clientId].roles">
+         <template v-for="client, name in roles">
+            <div v-if="name != 'global'"><h4>{{ client.title[lang] || name}}</h4>
+                <div class="input-role" v-for="role in client.roles">
                     <label :title="role.description.en">{{ role.title.en || role.name }}</label> 
-                    <span v-if="user.roles && user.roles[client.clientId] &&user.roles[client.clientId].indexOf(role.name) >= 0" style="color:green;" >
+                    <span v-if="user.roles && user.roles[name] &&user.roles[name].indexOf(role.name) >= 0" style="color:green;" >
                       <font-awesome-icon icon="fa-solid fa-check" /> 
                     </span>
                     <span v-else-if="role.status === 'WAITING'" title="Request being processed">
@@ -36,11 +49,12 @@
                         <font-awesome icon="fa-solid fa-close"></font-awesome>
                     </span>
                    
-                    <span v-else><input type="checkbox" v-model="checkedRoles" :value="client.clientId + '.' + role.name" /></span>
+                    <span v-else><input type="checkbox" v-model="checkedRoles" :value="name + '.' + role.name" /></span>
                 </div>
             </div>
 
         </template>
+        
         <h2>Motivation for the moderator</h2>
         <textarea v-model="message"></textarea>
        
@@ -65,6 +79,10 @@ export default {
             type: String,
             default: 'vso-opt'
         },
+        withInstitution: {
+            type:Boolean,
+            default: true
+        },
         api: {
             type: String,
             default: 'https://catalog.formater/api'
@@ -79,7 +97,9 @@ export default {
             clients: {},
             checkedRoles: [],
             message: '',
-            asking: false
+            asking: false,
+            success: null,
+            error: null
         }
     },
     computed: {
@@ -117,15 +137,14 @@ export default {
                 domain: location.href,
                 message: this.message,
                 role: this.checkedRoles,
-                lang: this.lang,
-                organizationId: this.organisation.id
+                lang: this.lang
             }
-            if (this.organisation.id) {
-                postdata['organizationId'] = this.organisation.id
-            } else {
-                postdata['organization'] = this.organisation.name
-                postdata['organizationType'] = this.organisation.types
-            }
+            // if (this.organisation.id) {
+            //     postdata['organizationId'] = this.organisation.id
+            // } else {
+            //     postdata['organization'] = this.organisation.name
+            //     postdata['organizationType'] = this.organisation.types
+            // }
             var fdata = new URLSearchParams(postdata)
             var url =  this.api.replace('/api', '/requests/ask')
             fetch(url,{
@@ -138,18 +157,32 @@ export default {
             }).then(resp => resp.json())
             .then(json => {
                 this.asking = false
-                this.success = json.success
+                if (json.success) {
+                    this.success = 'Your request has been successfully recorded.<br />An email has been sent to you. '
+                }
+                console.log(json)
                 if (json.success && json.roles) {
+                    var roles = this.roles
                     json.roles.forEach(function (role) {
-                        client.setRoleStatus(role, 'WAITING')
+                        var tab = role.split('.')
+                        console.log(tab)
+                        var index = roles[tab[0]].roles.findIndex(r => r.name === tab[1])
+                        roles[tab[0]].roles[index].status = 'WAITING'
+                      
+                        // client.setRoleStatus(role, 'WAITING')
                     })
+                    if (json.organizationId) {
+                        this.organisation.id = json.organizationId
+                    }
+                    this.roles = roles
                     this.checkedRoles = []
                     this.message = null
                 }
                 if (json.error) {
-                    this.error = json.error
+                    this.error = 'AN ERROR OCCURRED: <br />' + json.error
                 }
             }).catch((error) => {
+                console.log(error)
                 this.asking = false
                 this.error = 'SERVER ERROR'
             })
@@ -228,10 +261,7 @@ export default {
                 }
                 this.clients = json.clients
                 this.roles = json.roles
-                console.log(this.user.roles)
                 for(var client in this.roles) {
-                    console.log(client)
-                    console.log(this.roles[client])
                     if (client  !== 'global') {
                         this.roles[client].roles.forEach((role) => {
                             console.log(role)
@@ -240,11 +270,17 @@ export default {
                 }
 
             })
+        },
+        resetMessage() {
+            this.error = null
+            this.success = null
         }
     }
 }
 </script>
+
 <style>
+
 svg.svg-inline--fa {
   display: inline-block;
   height: 1em;
@@ -255,6 +291,20 @@ svg:not(:root).svg-inline--fa, svg:not(:host).svg-inline--fa {
   overflow: visible;
   box-sizing: content-box;
 }
+@keyframes fa-spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+.fa-spin {
+  font-size:50px;
+  -webkit-animation: fa-spin 2s linear infinite;
+  animation: fa-spin 2s linear infinite;
+}
+
 
 </style>
 <style scoped>
@@ -308,5 +358,37 @@ label {
 .access button:disabled {
     opacity:0.4;
     pointer-events:none;
+}
+div.spinner {
+    position:fixed;
+    top:0;
+    left:0;
+    width:100%;
+    height:100vh;
+    padding-left:calc(50% - 25px);
+   display: flex;
+   
+    align-items: center;
+    background:rgba(0,0,0,0.2);
+}
+div.spinner > div {
+    height:60px;
+    text-align:center;
+}
+div.message {
+    position: fixed;
+    top:calc(50vh - 100px );
+    margin-left:200px;
+    max-width:500px;
+    width: fit-content;
+    padding:30px;
+    background: white;
+    box-shadow: 0 0 3px rgba(0,0,0,.5);
+}
+div.message.success {
+    color:darkgreen;
+}
+div.message.error {
+    color:darkred;
 }
 </style>
--- src/fontawesome.js
+++ src/fontawesome.js
@@ -3,12 +3,12 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 /* import specific icons */
-import {faCheck,faClock, faClose} from '@fortawesome/free-solid-svg-icons'
+import {faCheck,faClock, faClose, faRefresh,faSpinner} from '@fortawesome/free-solid-svg-icons'
 
 import {
    faSquare,
    faSquareCheck
 } from '@fortawesome/free-regular-svg-icons'
-library.add(faCheck,faClock, faClose, faSquare, faSquareCheck)
+library.add(faCheck,faClock, faClose,faRefresh,faSpinner, faSquare, faSquareCheck)
 
 export {FontAwesomeIcon}
